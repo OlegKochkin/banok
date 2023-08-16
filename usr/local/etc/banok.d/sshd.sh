84b99f25bef1243407b4475ab339fbd235102488
Oleg Kochkin <39468044+OlegKochkin@users.noreply.github.com>
2023-08-16 16:52:03 +0300

    Update sshd.sh

--- usr/local/etc/banok.d/sshd.sh
+++ usr/local/etc/banok.d/sshd.sh
@@ -1,3 +1,6 @@
 MIN=$(date +%M -d "-10 minutes")
 DateFilter="$(date +%Y-%m-%dT%H: -d "-10 minutes")""${MIN:0:1}"
-grep $DateFilter /var/log/secure | grep -e "Invalid user" | awk '{print $8}' | sort | uniq | Add "SSH Brute-Force. Ports scanning" "15,14,18,22"
+grep $DateFilter /var/log/secure | grep -e "Invalid user" | grep -v "  " | awk '{print $8}' | sort | uniq | Add "SSH Brute-Force. Ports scanning" "15,14,18,22"
+grep $DateFilter /var/log/secure | grep -e "from invalid user" | awk '{print $9}' | sort | uniq | Add "SSH Brute-Force. Ports scanning" "15,14,18,22"
+grep $DateFilter /var/log/secure | grep -e "by invalid user" | grep -v "  " | awk '{print $10}' | sort | uniq | Add "SSH Brute-Force. Ports scanning" "15,14,18,22"
+grep $DateFilter /var/log/secure | grep -e "for invalid user" | grep -v "  " | awk '{print $14}' | sort | uniq | Add "SSH Brute-Force. Ports scanning" "15,14,18,22"
